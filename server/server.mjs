f8e99110ed23656e97375ff99c1387a387151fe2
QUYẾT TIẾN <152900541+HoTien2004@users.noreply.github.com>
2025-01-27 16:39:03 +0700

    2. What is Schema & Resolver in GraphQL ?

--- server/server.mjs
+++ server/server.mjs
@@ -4,12 +4,32 @@ import { ApolloServer } from '@apollo/server';
 import { ApolloServerPluginDrainHttpServer } from  '@apollo/server/plugin/drainHttpServer';
 import bodyParser from 'body-parser';
 import { expressMiddleware } from '@apollo/server/express4';
+import cors from 'cors';
 
 const app = express();
 const httpServer = http.createServer(app);   
 
-const typeDefs = '';
-const resolvers = {};
+const typeDefs = `#graphql
+    type Folder {
+        id: String,
+        name: String,
+        created: String,
+    }
+
+    type Author {
+        id: String,
+        name: String,
+    }
+
+    type Query {
+        folders: [Folder]
+    }
+`;
+const resolvers = {
+    Query: {
+        name: () => { return 'Hello World!' }
+    }
+};
 
 const server = new ApolloServer({
     typeDefs,
