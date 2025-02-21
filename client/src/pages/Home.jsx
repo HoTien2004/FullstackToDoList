16a95923d37bb6aa4a8d4233610d0be2ab7f9265
QUYẾT TIẾN <152900541+HoTien2004@users.noreply.github.com>
2025-02-21 17:30:31 +0700

    23. Add Real time Push Notification function using WebSocket

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import { Box, Grid, Typography } from '@mui/material'
 import UserMenu from '../components/UserMenu'
 import FolderList from '../components/FolderList'
 import { Outlet, useLoaderData } from 'react-router-dom'
+import PushNotification from '../components/PushNotification'
 
 
 const Home = () => {
@@ -14,6 +15,7 @@ const Home = () => {
       <Typography variant='h4' sx={{ mb: '20px' }}>Note App</Typography>
       <Box sx={{ display: 'flex', justifyContent: 'right', mb: '10px' }}>
         <UserMenu />
+        <PushNotification />
       </Box>
 
       <Grid container sx={{ height: '50vh', boxShadow: '0 0 15px 0 rgb(193 193 193 / 60%)' }}>
--- server/server.mjs
+++ server/server.mjs
@@ -6,12 +6,14 @@ import bodyParser from 'body-parser';
 import { expressMiddleware } from '@apollo/server/express4';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import { makeExecutableSchema } from '@graphql-tools/schema';
+import { WebSocketServer } from 'ws';
+import { useServer } from 'graphql-ws/lib/use/ws';
 import 'dotenv/config'
 import { resolvers } from './resolvers/index.js';
 import { typeDefs } from './schemas/index.js';
 import './firebaseConfig.js'
 import { getAuth } from 'firebase-admin/auth'
-import { decode } from 'punycode';
 
 const app = express();
 const httpServer = http.createServer(app);   
@@ -20,10 +22,34 @@ const httpServer = http.createServer(app);
 const URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@note-app.t6us4.mongodb.net/?retryWrites=true&w=majority&appName=note-app`
 const PORT = process.env.PORT || 4000;
 
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+// Creating the WebSocket server
+const wsServer = new WebSocketServer({
+    // This is the `httpServer` we created in a previous step.
+    server: httpServer,
+    // Pass a different path here if app.use
+    // serves expressMiddleware at a different path
+    path: '/',
+  });
+  
+  // Hand in the schema we just created and have the
+  // WebSocketServer start listening.
+  const serverCleanup = useServer({ schema }, wsServer);
+
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })]
+    schema,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer }),
+        {
+            async serverWillStart() {
+              return {
+                async drainServer() {
+                  await serverCleanup.dispose();
+                },
+              };
+            },
+        },
+    ]
 })
 
 await server.start();
@@ -44,7 +70,7 @@ const authorizationJWT = async (req, res, next) => {
             return res.status(403).json({message: 'Forbidden', error: err});
         })
     } else {
-        return res.status(401).json({message: 'Unauthorized'});
+        next();
     }
 }
 
